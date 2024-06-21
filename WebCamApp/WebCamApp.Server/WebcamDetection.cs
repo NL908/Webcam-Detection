27fa6a765bd26fefa0ffc5fd4128d4ab8d31100d
Nanqi-Liu <fafalnq@gmail.com>
2024-06-21 12:17:26 -0400

    Fix camera can't frame grab bug

--- WebCamApp/WebCamApp.Server/WebcamDetection.cs
+++ WebCamApp/WebCamApp.Server/WebcamDetection.cs
@@ -18,41 +18,47 @@ app.Map("/ws", async context =>
     if (context.WebSockets.IsWebSocketRequest)
     {
         using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-        using (VideoCapture capture = new(0))
+        using Mat srcImage = new Mat();
+        using VideoCapture capture = new(0);
+        capture.Read(srcImage);
+        while (true)
         {
-            while (true)
+            // Read new frame
+            bool bSuccess = capture.Read(srcImage);
+
+            // Camera might need time to start up
+            // Break if no frame has been grabbed.
+            if (!bSuccess)
             {
-                // Get the camera mat
-                if (capture.IsOpened())
-                using (Mat srcImage = capture.RetrieveMat())
-                    if (!srcImage.Empty())
-                        // Clone the source image to the output image
-                        using (Mat afterImage = srcImage.Clone())
-                        {
-                            FaceAndEyeDetection detection = new();
-                            // Apply face & eye detection and draw results on the output image
-                            detection.FaceDetect(srcImage, afterImage, out _);
-                            detection.EyeDetect(srcImage, afterImage, out _);
-
-                            // Store the source image and result into a class for json parsing
-                            DetectionData data = detection.MatsToBase64(srcImage, afterImage);
-                            string jsonMessage = JsonSerializer.Serialize<DetectionData>(data);
-
-                            // Send the string json data through websocket
-                            byte[] bytes = Encoding.UTF8.GetBytes(jsonMessage);
-                            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
-                            if (webSocket.State == WebSocketState.Open)
-                            {
-                                await webSocket.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
-                            }
-                            else if (webSocket.State == WebSocketState.Closed || webSocket.State == WebSocketState.Aborted)
-                            {
-                                break;
-                            }
-                        }
-                // Wait for 0.1 second between each frame
-                Thread.Sleep(100);
+                break;
             }
+            
+            // Clone the source image to the output image
+            using (Mat afterImage = srcImage.Clone())
+            {
+                FaceAndEyeDetection detection = new();
+                // Apply face & eye detection and draw results on the output image
+                detection.FaceDetect(srcImage, afterImage, out _);
+                detection.EyeDetect(srcImage, afterImage, out _);
+
+                // Store the source image and result into a class for json parsing
+                DetectionData data = detection.MatsToBase64(srcImage, afterImage);
+                string jsonMessage = JsonSerializer.Serialize<DetectionData>(data);
+
+                // Send the string json data through websocket
+                byte[] bytes = Encoding.UTF8.GetBytes(jsonMessage);
+                var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+                if (webSocket.State == WebSocketState.Open)
+                {
+                    await webSocket.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+                else if (webSocket.State == WebSocketState.Closed || webSocket.State == WebSocketState.Aborted)
+                {
+                    break;
+                }
+            }
+            // Wait for 0.1 second between each frame
+            Thread.Sleep(100);
         }
     }
     else
